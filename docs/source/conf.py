66e1894fa2c5034c44574567240d54fc3f54bb6b
Rick Spencer <rick@influxdata.com>
2024-01-28 08:06:03 -0500

    generating documentation with sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src'))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -14,7 +18,10 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    # ... any other extensions you have enabled ...
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
